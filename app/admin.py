585fd8e5b0f73821a71d7ca04c23174afd4cb470
netcat <974123274@qq.com>
2023-07-02 14:17:20 +0300

    add db.sqlite3

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Statement
+
+class StatementModelAdmin(admin.ModelAdmin):
+    # 在这里定义你的模型管理选项
+    pass
+
+admin.site.register(Statement, StatementModelAdmin)
+
+
